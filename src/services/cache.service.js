349795a519b66da7882026d1c54279757f2f6a02
Alex Ortega <alexortegazj@gmail.com>
2021-09-18 15:16:53 -0700

    Getting cache from database

--- src/services/cache.service.js
+++ src/services/cache.service.js
@@ -1,10 +1,20 @@
+const UserRepository = require('../shared/repositories/user.repository');
+const CacheRepository = require('../shared/repositories/cache.repository');
+
 /**
  * Get All user cache
  * @param {ObjectId} cognitoId
  * @returns {Promise<Cache>}
  */
 const getAll = async (cognitoId) => {
-  return [cognitoId];
+  const user = await UserRepository.findOneByCognitoId(cognitoId);
+  let cache = [user];
+
+  if (user && user.userId) {
+    cache = await CacheRepository.findAllByUserId(user.userId);
+  }
+
+  return cache;
 };
 
 module.exports = {
